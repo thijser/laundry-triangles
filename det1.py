30225325de951c4450da5bfce1d7365d993f6ffe
thijs <thijsboumans94@gmail.com>
2016-03-25 13:11:22 +0100

    premerge

--- det1.py
+++ det1.py
@@ -4,74 +4,71 @@ import random as rand
 import math
 
 
-class Graph
-	def__init__(self,nodes)
-		self.nodes=nodes
-		self.lowest = min(i.x for i in nodes)
-		
-		
-class GraphNode
-	def__init__ (self,x,y)
-		self.x= x	
-		self.y= y 
+class Graph:
+    def __init__(self,nodes):
+        self.nodes=nodes
 
-		self.edges=[]
-	
-def dinvandconquer (points)
-	sorted(points, key=itemgetter(0))
+        
+        
+class GraphNode:
+    def __init__ (self,x,y):
+        self.x= x    
+        self.y= y 
 
-	
-def merge(left,right)
-	
-	
-	
-	
-	
-	
-	
-	
+        self.edges=[]
+    
+def dinvandconquer (points):
+     return divid(sorted(points, key=itemgetter(0)))
+    
+def merge(left,right):
+    left.nodes[0].edges.append(right.nodes[0]);
+
+    return Graph(mergelist(left.nodes,right.nodes));
+     
+    
+    
 def mergeLists(left,right):
-	if(left[len(left)-1])>right[len(right)-1]:
-		left,right = right,left
-	res=[]
-	j=0;
-	i=0;
-	
-	while(i<len(right)):
-		if(left[i][1]<right[j][1]):
-			res[i+j]=left[i]
-			i=i+1;
-		else:
-			res[i+j]=right[j]
-			j=j+1;		
-				
-		
+    if(left[len(left)-1])>right[len(right)-1]:
+        left,right = right,left
+    res=[]
+    j=0;
+    i=0;
+    
+    while(i<len(right)):
+        if(left[i][1]<right[j][1]):
+            res[i+j]=left[i]
+            i=i+1;
+        else:
+            res[i+j]=right[j]
+            j=j+1;        
+                
+        
 
 def divid(sortedPoints):
-	
-	if(len(sortedPoints<3)):
-		a = GraphNode(sortedpoints[0][0],sortedpoints[0][1])
-		b = GraphNode(sortedpoints[1][0],sortedpoints[1][1])
-		a.edges[0]=b 
-		b.edges[0]=a
-		return Graph([a,b])
-		
-	if(len(sortedPoints<4)):
-		a = GraphNode(sortedpoints[0][0],sortedpoints[0][1])
-		b = GraphNode(sortedpoints[1][0],sortedpoints[1][1])
-		c= GraphNode(sortedpoints[2][0],sortedpoints[2][1])
-				
-		a.edges[0]=b 
-		a.edges[1]=c
-		b.edges[0]=a
-		b.edges[1]=c
-		c.edges[0]=a
-		c.edges[0]=b 
-		return Graph([a,b,c])
-		
+    
+    if(len(sortedPoints<3)):
+        a = GraphNode(sortedpoints[0][0],sortedpoints[0][1])
+        b = GraphNode(sortedpoints[1][0],sortedpoints[1][1])
+        a.edges[0]=b 
+        b.edges[0]=a
+        return Graph([a,b])
+        
+    if(len(sortedPoints<4)):
+        a = GraphNode(sortedpoints[0][0],sortedpoints[0][1])
+        b = GraphNode(sortedpoints[1][0],sortedpoints[1][1])
+        c= GraphNode(sortedpoints[2][0],sortedpoints[2][1])
+                
+        a.edges[0]=b 
+        a.edges[1]=c
+        b.edges[0]=a
+        b.edges[1]=c
+        c.edges[0]=a
+        c.edges[0]=b 
+        return Graph([a,b,c])
+        
 
-	left = sortedPoints[:len(A)/2]
-	right = sortedPoints[len(A)/2:]
-	return merge(divid(left),divid(right))
+    left = sortedPoints[:len(A)/2]
+    right = sortedPoints[len(A)/2:]
+    return merge(divid(left),divid(right))
 
-	
+    
--- test.py
+++ test.py
@@ -5,13 +5,15 @@ import matplotlib.pyplot as plt
 from triangulation import chew_triangulation
 from triangulation import circumcircle
 from triangulation import add_triangle
+from det1 import dinvandconquer
 
 from polygons import create_circular_polygon
 
 triangle = [[3,0], [3,3], [0,0]]
+chew_triangulation
+polygon=create_circular_polygon(70,20)
 
-triangles = chew_triangulation(create_circular_polygon(70,20))
-#triangles = chew_triangulation([(-4,-2),(-3,-2),(2,2),(5,2),(-2,-3)])
+triangles = chew_triangulation(polygon)
 graph = nx.Graph()
 pos = {}
 
@@ -21,3 +23,14 @@ for triangle in triangles:
 nx.draw(graph, pos)
 
 plt.show()
+
+pos={}
+g = dinvandconquer(polygon)
+graph= nx.Graph()
+for nod in g.nodes
+   graph.add_node(nod.x+","+nod.y)
+   pos[nod.x+","+nod.y]=[nod.x,nod.y]
+for node in g.nodes
+    for edge in node.edges
+        graph.add_edge((nod.x+","+nod.y),(edge.x+","+edge.y))
+nx.draw(graph,pos)
