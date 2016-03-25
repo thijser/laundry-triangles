66c74f8288e19729a1055483692d8da68978dab5
thijs <thijsboumans94@gmail.com>
2016-03-25 15:58:05 +0100

    better ?

--- det1.py
+++ det1.py
@@ -2,6 +2,7 @@ from operator import itemgetter
 import networkx as nx
 import random as rand
 import math
+import triangulation as tr
 
 
 
@@ -12,7 +13,6 @@ class GraphNode:
     def __init__ (self,x,y):
         self.x= x    
         self.y= y 
-
         self.edges=[]
     
 def dinvandconquer (points):
@@ -20,7 +20,34 @@ def dinvandconquer (points):
      return divid(sorted(points, key=itemgetter(0)))
     
 def merge(left,right):
+    left[0].edges.add(right[0])
+    i=0
+    j=0
+    a=left[0]
+    b=right[0]
+    c=left[0]
 
+    while((i<len(left)-1)&(j<len(right)-1)):
+        circle=tr.circumcircle([[a.x,a.y],[b.x,b.y],[left[i+1].x,left[i+1].x]])
+        if (tr.in_circle(circle,[right[i+j].x,right[i+1].y])):
+            c=left[j+1]
+            c.edges.add(b)
+            c.edges.add(a)
+            b.edges.add(c)
+            a.edges.add(c)
+            a=c
+            j=j+1
+        else:
+            c=right[i+1]
+            c.edges.add(b)
+            c.edges.add(a)
+            b.edges.add(c)
+            a.edges.add(c)
+            a=c
+            i=i+1
+            
+    left[len(left)-1].edges.add(right[len(right)-1])
+    right[len(right)-1].edges.add(left[len(left)-1])
     return mergeLists(left,right)
    
     
