03224db3f7ad0d06faf606501602da930be69bb4
Jorik Oostenbrink <jorik@oostenbrink.org>
2016-03-25 13:51:24 +0100

    adding polygons

--- polygons.py
+++ polygons.py
@@ -4,12 +4,20 @@ import math
 from decimal import *
 
 def create_circular_polygon(radius, num_points):
-    func = lambda p : (Decimal(math.cos(p*2*math.pi)*radius), Decimal(math.sin(p*2*math.pi)*radius))
-       
+    return create_ellipsular_polygon(radius, radius, num_points)
+
+def create_ellipsular_polygon(a, b, num_points):
+    func = lambda p : (Decimal(a*math.cos(p*2*math.pi)), Decimal(b*math.sin(p*2*math.pi)))
+    
     return create_polygon(func, num_points)
- 
+    
 def create_polygon(func, num_points):    
     points = [rand.random() for i in range(0,num_points)]
     points.sort()
     
     return [func(p) for p in points]
+    
+def create_random_structure(num_points):
+    mult = 50;
+    return [(Decimal(rand.random()*50), Decimal(rand.random()*50)) for i in range(0,num_points)]
+    
