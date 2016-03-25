554af2aacd971380ab4c01b2ca887909306f34f4
Jorik Oostenbrink <jorik@oostenbrink.org>
2016-03-25 15:11:55 +0100

    fix

--- test2.py
+++ test2.py
@@ -11,12 +11,16 @@ pos={}
 polygon=create_circular_polygon(20,30)
 g = dinvandconquer(polygon)
 graph= nx.Graph()
-for nod in g:
-   graph.add_node('{0:f}'.format(nod.x)+","+'{0:f}'.format(nod.y))
-   pos['{0:f}'.format(nod.x)+","+'{0:f}'.format(nod.y)]=['{0:f}'.format(nod.x),'{0:f}'.format(nod.y)]
+
+for node in g:
+   graph.add_node((node.x, node.y))
+   pos[(node.x, node.y)] = [float(node.x), float(node.y)]
+   
 for node in g:
     for edge in node.edges:
-        graph.add_edge(('{0:f}'.format(nod.x)+","+'{0:f}'.format(nod.y)),('{0:f}'.format(edge.x)+","+'{0:f}'.format(edge.y)))
-nx.draw(graph,pos)
+        graph.add_edge((node.x, node.y), (edge.x, edge.y))
+        
+nx.draw_networkx(graph, pos, with_labels = False)
+plt.show()
 
 
