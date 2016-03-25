1ac2f306bf8c95b58add730cdee83a18a65e4e5e
thijs <thijsboumans94@gmail.com>
2016-03-25 13:34:55 +0100

    arme computer

--- test.py
+++ test.py
@@ -13,24 +13,15 @@ import time
 sizes = []
 times = []
 multiplier = 20
-<<<<<<< HEAD
-n = 300
-=======
 n = 20
 repetitions = 10
->>>>>>> 1bf9ac19a8e8bb1fbeaf162086107ead0cc2f179
+
 
 sys.setrecursionlimit((n+5) * multiplier)
 
 for i in range(1,n):
     size = i*multiplier
-<<<<<<< HEAD
-    pol = create_circular_polygon(20,size) #For testing multiple algs we should first copy this
-    start_time = time.clock()
-    draw(chew_triangulation(pol))
-    end_time = time.clock()    
-    times.append(end_time - start_time)
-=======
+
     
     temp_times = []
     
@@ -42,7 +33,6 @@ for i in range(1,n):
         temp_times.append(end_time - start_time)
     
     times.append(sum(temp_times) / (float(repetitions)))
->>>>>>> 1bf9ac19a8e8bb1fbeaf162086107ead0cc2f179
     sizes.append(size)
     print(times[i-1])
 
