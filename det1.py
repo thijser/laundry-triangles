68105dffb6c272b9de36440c6cb9511dbe5590ee
thijs <thijsboumans94@gmail.com>
2016-03-27 11:23:35 +0200

    bug fixes

--- det1.py
+++ det1.py
@@ -14,7 +14,13 @@ class GraphNode:
         self.x= x    
         self.y= y 
         self.edges=[]
-    
+    def __repr__(self):
+        st= '{0:.{1}f}'.format((self.x),2),",",'{0:.{1}f}'.format((self.y),2), ",<"
+        for e in self.edges:
+           st+= "(",'{0:.{1}f}'.format((e.x),2),",",'{0:.{1}f}'.format((e.y),2),")"
+        st+=">",
+        return ''.join((st))
+
 def dinvandconquer (points):
 
      return divid(sorted(points, key=itemgetter(0)))
@@ -26,10 +32,17 @@ def merge(left,right):
     a=left[0]
     b=right[0]
     c=left[0] #candidate 
+    print("left:")
+    for e in left:
+        print(e.y)
+    print("right")
+    for e in right:
+        print(e.y)
 
-    while((i<len(left)-1)&(j<len(right)-1)):
+    while((i<len(left)-2)&(j<len(right)-2)):
         circle=tr.circumcircle([[a.x,a.y],[b.x,b.y],[left[i+1].x,left[i+1].x]])
-        if (tr.in_circle(circle,[right[i+j].x,right[i+1].y])):
+
+        if (tr.in_circle(circle,[right[i+1].x,right[i+1].y])):
             c=left[j+1]
             j=j+1
             circle2=tr.circumcircle([[a.x,a.y],[b.x,b.y],[c.y,c.x]])
@@ -89,19 +102,26 @@ def divid(sortedPoints):
         b = GraphNode(sortedPoints[1][0],sortedPoints[1][1])
         a.edges={b} 
         b.edges={a}
-        return [a,b]
-        
+        print("ja")
+        if(a.y>b.y):
+            print(b.x,",",b.y,"-",a.x,",",a.y)
+            return [b,a]
+        print(a.x,",",a.y,"-",b.x,",",b.y)
+        return[a,b]
     if(len(sortedPoints)<4):
         a = GraphNode(sortedPoints[0][0],sortedPoints[0][1])
         b = GraphNode(sortedPoints[1][0],sortedPoints[1][1])
         c= GraphNode(sortedPoints[2][0],sortedPoints[2][1])
-                
         a.edges={b,c}
         b.edges={a,c}
         c.edges={a,b}
+        if(a.y>b.y):
+           a,b=b,a 
+        if(a.y>c.y):
+           a,c=c,a
+        if(b.y>c.y):
+           b,c=c,b
         return [a,b,c]
-        
-
     left = sortedPoints[:int(len(sortedPoints)/2)]
     right = sortedPoints[int(len(sortedPoints)/2):]
  
--- test2.py
+++ test2.py
@@ -13,11 +13,15 @@ g = dinvandconquer(polygon)
 graph= nx.Graph()
 
 for node in g:
+   print(node.x,",",node.y)
    graph.add_node((node.x, node.y))
    pos[(node.x, node.y)] = [float(node.x), float(node.y)]
    
+print("edges:")
 for node in g:
     for edge in node.edges:
+        print(node.x,",", node.y, "<<node")
+        print(edge.x,",",edge.y, "<<edge")
         graph.add_edge((node.x, node.y), (edge.x, edge.y))
         
 nx.draw_networkx(graph, pos, with_labels = False)
